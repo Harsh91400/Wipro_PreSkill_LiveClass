4ec287bcb77ce95fb1bca1e5bb2b34b7df4edf19
Harsh91400 <140680081+Harsh91400@users.noreply.github.com>
2025-11-13 01:09:12 +0530

    Add files via upload

---
+++ Date_12_11/JdbcExample/src/main/java/com/java/wipro/EmployUpdateMain.java
@@ -0,0 +1,49 @@
+package com.java.wipro;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class EmployUpdateMain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int empno;
+		String name,gender,dept,desig;
+		double basic;
+		System.out.println("Enter Employee No : ");
+		empno = sc.nextInt();
+		System.out.println("Enter Employee Name : ");
+		name = sc.next();
+		System.out.println("Enter Employee Gender(Male/Female) : ");
+		gender = sc.next();
+		System.out.println("Enter Employee Department : ");
+		dept = sc.next();
+		System.out.println("Enter Employee Designation : ");
+		desig = sc.next();
+		System.out.println("Enter Employee Basic : ");
+		basic = sc.nextDouble();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/wipronov25","root","Harsh@9140");
+			String cmd = "Update Employ set Name = ?, Gender = ?, Dept = ?, Desig = ?, Basic = ? where Empno = ?";
+			PreparedStatement pst = conn.prepareStatement(cmd);
+			pst.setString(1, name);
+			pst.setString(2, gender);
+			pst.setString(3, dept);
+			pst.setString(4, desig);
+			pst.setDouble(5, basic);
+			pst.setInt(6, empno);
+			pst.executeUpdate();
+			System.out.println("Record Updated...");
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
